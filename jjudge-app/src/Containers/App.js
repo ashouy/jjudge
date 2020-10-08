a66f824972d214d0241e4feda607927355e19d2a
ashouy <joaopaulokad@gmail.com>
2020-10-08 09:44:05 -0300

    #37 create main screen (new layout)

--- jjudge-app/src/Containers/App.js
+++ jjudge-app/src/Containers/App.js
@@ -1,26 +1,184 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from 'react'
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import SubmitSolution from '../components/MainContent/Solution/SubmitSolution'
+import Home from '../components/MainContent/Home'
+import CodeIcon from '@material-ui/icons/Code';
+const drawerWidth = 240;
 
-import Screen from '../components/MainContent/submitSolution/SubmitScreen'
-import classes from './App.module.css'
-import Home from '../components/Home/Home'
-import Header from '../components/Header/Header'
-class App extends Component {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
 
-    render() {
-        return (
-            <BrowserRouter>
-                <div id="app-div" className={classes.App}>
-                    <Header />
-                    <div id="main-content-div" className={classes.MainContent}>
-                        <Route path="/" exact component={Home} />
-                        <Route path="/submitSolution" component={Screen} />
-                    </div>
-                </div>
-            </BrowserRouter>
 
-        )
-    }
+function ListItemLink(props) {
+    const { icon, primary, to } = props;
+    const CustomLink = React.useMemo(
+        () =>
+            React.forwardRef((linkProps, ref) => (
+                <Link ref={ref} to={to} {...linkProps} />
+            )),
+        [to],
+    )
+    return (
+        <li>
+            <ListItem button component={CustomLink} key={primary}>
+                <ListItem>{icon}</ListItem>
+                <ListItemText primary = {primary}/>
+            </ListItem>
+        </li>
+    )
 }
+
+const App = () => {
+
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+
+
+    return (
+        <BrowserRouter>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                            className={clsx(classes.menuButton, {
+                                [classes.hide]: open,
+                            })}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap>
+                            Mini variant drawer
+                </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    className={clsx(classes.drawer, {
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    })}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: open,
+                            [classes.drawerClose]: !open,
+                        }),
+                    }}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        <ListItemLink to="/" primary="Home" icon= {<HomeIcon/>}/>
+                        <ListItemLink to="submitSolution" primary="Solução" icon={<CodeIcon/>}/>
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/submitSolution" exact component={SubmitSolution}></Route>
+                </main>
+            </div>
+        </BrowserRouter>
+    )
+}
+
 
 export default App
---
+++ jjudge-app/src/components/MainContent/Solution/SubmitSolution.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubmitSolution = props =>{
+    return(
+        <div>
+            <h1>submit screen</h1>
+        </div>
+    )
+}
+
+export default  SubmitSolution
