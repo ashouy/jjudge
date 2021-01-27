ab091a3d7bc5379a67bfdf56ece7acb2ab609f20
ashouy <joaopaulokad@gmail.com>
2021-01-27 10:42:44 -0300

    #95 some silly changes

--- jjudege-server/src/useCases/CreateSolution/DoAvaliation.js
+++ jjudege-server/src/useCases/CreateSolution/DoAvaliation.js
@@ -1,6 +1,5 @@
 const axios = require('axios')
 const { uuid } = require('uuidv4')
-const sequelize = require('../../database/dbInstance')
 const { updateAvaliationState, updateAvaliationResult } = require('../../entities/Avaliation')
 
 var avaliationList = []
@@ -20,11 +19,11 @@ const runCode = async (solution, testCases) => {
                 'https://api.jdoodle.com/v1/execute',
                 program
             )
-            console.log(output.data)
-            console.log('saída')
-            console.log(output.data.output.toString())
-            console.log('saída esperada')
-            console.log(testCases[i].expectedOutput.toString())
+            // console.log(output.data)
+            // console.log('saída')
+            // console.log(output.data.output.toString())
+            // console.log('saída esperada')
+            // console.log(testCases[i].expectedOutput.toString())
             if (output.data.output.toString() != testCases[i].expectedOutput.toString()) {
                 return 1
             }
@@ -38,12 +37,12 @@ const runCode = async (solution, testCases) => {
 const updateAvaliation = async avaliation => {
 
     try {
-        const result = await sequelize.transaction(async (t) => {
-            await updateAvaliationState(1, avaliation.avaliation.id, t) //muda status para running
-            var result = await runCode(avaliation.solution, avaliation.testCases) //retorna o resultado da execução
-            await updateAvaliationResult(result, avaliation.avaliation.id, t) //altera o resultado da avaliação
-            await updateAvaliationState(2, avaliation.avaliation.id, t) //muda status para finished
-        })
+        //depende de transação?
+        await updateAvaliationState(1, avaliation.avaliation.id, updateStateTransaction) //muda status para running
+        var result = await runCode(avaliation.solution, avaliation.testCases) //retorna o resultado da execução
+        await updateAvaliationResult(result, avaliation.avaliation.id, t) //altera o resultado da avaliação
+        await updateAvaliationState(2, avaliation.avaliation.id, t) //muda status para finished
+
     } catch (error) {
         console.log(error)
     }
