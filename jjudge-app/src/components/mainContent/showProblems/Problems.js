5df8863247388d0aa4cfce6d038b0b301a13efb0
ashouy <joaopaulokad@gmail.com>
2021-02-10 17:33:00 -0300

    #97 start show problens screen

--- jjudge-app/src/components/mainContent/showProblems/Problems.js
+++ jjudge-app/src/components/mainContent/showProblems/Problems.js
@@ -1,15 +1,123 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button, TableFooter } from '@material-ui/core';
+import TablePagination from '@material-ui/core/TablePagination'
 
-const Problems = props =>{
+import IconButton from '@material-ui/core/IconButton'
+import InfoIcon from '@material-ui/icons/Info';
 
-    return(
-        <div>
-            <Typography>
-                alot of problems
-            </Typography>
-        </div>
-    )
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+const useStyles1 = makeStyles((theme) => ({
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+    },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Icekhagfygafhal', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbreaaaaad', 356, 16.0, 49, 3.9),
+];
+
+
+
+const Problems = props => {
+
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table" size='small'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align='left' >Title</TableCell>
+                        <TableCell align='left'>Info</TableCell>
+                        <TableCell align="right">Code</TableCell>
+                        <TableCell align="right">Tag</TableCell>
+                        <TableCell align="right">Level</TableCell>
+                        <TableCell align="right">Rate</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {(rowsPerPage > 0
+                        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : rows
+                    ).map((row) => {
+                        return (
+                            <TableRow
+                                hover
+                                key={row.name}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align='left'>
+                                    <IconButton >
+                                        <InfoIcon />
+                                    </IconButton>
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            component='div'
+                            // colSpan={3}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default Problems
